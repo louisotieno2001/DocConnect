7a30d1b16b47d2287f504e9426a80b619c47f39d
LouisO <louisotieno2001@gmail.com>
2023-06-11 12:25:05 +0300

    Corrected the tabs layout to a more optimum code

--- app/src/main/java/com/healthcare/docconnect/HomeActivity.java
+++ app/src/main/java/com/healthcare/docconnect/HomeActivity.java
@@ -1,14 +1,26 @@
 package com.healthcare.docconect;
 
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-import android.os.Bundle;
 import com.google.android.material.tabs.TabLayout;
 
-public class HomeActivity extends AppCompatActivity {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.healthcare.docconect.fragments.AppointmentsMainFragment;
+import com.healthcare.docconect.fragments.MessagesMainFragment;
+import com.healthcare.docconect.ContactsMainFragment;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, View.OnClickListener{
 
     private ViewPager viewPager;
-    private TabLayout tabLayout;
+    private ArrayList<FragmentItem> tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +30,18 @@ public class HomeActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.view_pager);
         tabLayout = findViewById(R.id.tab_layout);
 
-        // Create an instance of the adapter that will control the fragments shown in each tab
-        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.addOnPageChangeListener(this);
 
-        // Add the fragments to the adapter
-        tabAdapter.addFragment(new ContactsFragment(), "Contacts");
-        tabAdapter.addFragment(new MessagesFragment(), "Messages");
-        tabAdapter.addFragment(new AppointmentsFragment(), "Appointments");
+        tabLayout = new ArrayList<FragmentItem>();
 
-        // Set the adapter to the ViewPager
-        viewPager.setAdapter(tabAdapter);
+        tabLayout.add(FragmentItem.newItem("Contacts", ContactsMainFragment.newInstance()));
+        tabLayout.add(FragmentItem.newItem("Messages", MessagesMainFragment.newInstance()));
+        tabLayout.add(FragmentItem.newItem("Appointments", AppointmentsMainFragment.newInstance()));
 
-        // Connect the TabLayout with the ViewPager
-        tabLayout.setupWithViewPager(viewPager);
+        MyAdapter adapter = new MyAdapter(getSupportFragmentManager(), tabLayout);
+        viewPager.setAdapter(adapter);
+    
+
+        
     }
 }
