07b28230864fde7bdb9d2e95fdf44c8bf5fb5d95
LouisO <louisotieno2001@gmail.com>
2023-06-23 20:27:39 +0300

    Updated the splash  activity to final code

--- app/java/com/healthcare/docconnect/SettingsActivity.java
+++ app/java/com/healthcare/docconnect/SettingsActivity.java
@@ -40,5 +40,7 @@ public class SettingsActivity extends AppCompatActivity{
         });
 
         
+
+        
     }
 }
--- app/java/com/healthcare/docconnect/SplashActivity.java
+++ app/java/com/healthcare/docconnect/SplashActivity.java
@@ -1,13 +1,31 @@
 package com.healthcare.docconnect;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    // Duration of splash screen in milliseconds
+    private static final long SPLASH_DURATION = 3000; // 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Create a handler to delay the transition to the main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                // Finish the splash activity
+                finish();
+            }
+        }, SPLASH_DURATION);
     }
 }
