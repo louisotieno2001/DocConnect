19e6c95b9e6644fcc3f51b5dafc44c86121262cc
LouisO <louisotieno2001@gmail.com>
2023-06-24 18:05:41 +0300

    Added firebase authentication and dtorage for user data in settings actvitiy and main activity

--- app/java/com/healthcare/docconnect/MainActivity.java
+++ app/java/com/healthcare/docconnect/MainActivity.java
@@ -5,14 +5,21 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-     //Declaring views
-     EditText phoneEdit;
-     EditText localityEdit;
-     TextView nextStepText;
+    //Declaring views
+    EditText phoneEdit;
+    EditText localityEdit;
+    TextView nextStepText;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = auth.getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +31,49 @@ public class MainActivity extends AppCompatActivity {
         localityEdit = findViewById(R.id.locality_edit);
         nextStepText = findViewById(R.id.next_step);
 
+       
+
 
         //Handling data input in the EditText
+        nextStepText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Get the input values from EditText fields
+                String phoneNumber = phoneEdit.getText().toString().trim();
+                String locality = localityEdit.getText().toString().trim();
 
-        //Click listener to proceed to next
+                // Get the user ID
+                String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        
+                // Initialize the DatabaseReference for the user
+                DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+                DatabaseReference userRef = usersRef.child(userId);
+              
+        
+                // Save the input values to the user's profile in Firebase Realtime Database
+                userRef.child("phoneNumber").setValue(phoneNumber);
+                userRef.child("locality").setValue(locality);
+        
+                // Show a success message or perform any additional actions
+                Toast.makeText(MainActivity.this, "Details saved successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+    
+
       
     }
+
+    private String getCurrentUserId() {
+
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            if (currentUser != null) {
+                  String userId = currentUser.getUid();
+                   return userId;
+          } else {
+        // User is not authenticated or session expired
+        // Handle the situation accordingly
+        return null;
+        }
+   }
 }
--- app/java/com/healthcare/docconnect/SettingsActivity.java
+++ app/java/com/healthcare/docconnect/SettingsActivity.java
@@ -1,13 +1,32 @@
 package com.healthcare.docconnect;
 
-import android.os.Bundle;
-import android.view.View;
-import android.content.Intent;
 import android.widget.TextView;
 import android.content.Context;
 import android.widget.EditText;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingsActivity extends AppCompatActivity{
    //Declaring views
@@ -17,6 +36,9 @@ public class SettingsActivity extends AppCompatActivity{
    EditText phoneNumberEdit;
    EditText localityEdit;
    TextView  saveSettingText;
+   private static final int REQUEST_CODE_IMAGE = 1;
+   private static final int REQUEST_CODE_PERMISSION = 2;
+   private StorageReference storageReference;
 
    @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +52,24 @@ public class SettingsActivity extends AppCompatActivity{
         localityEdit = findViewById(R.id.locality_edit);
         saveSettingText = findViewById(R.id.save_text);
 
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(SettingsActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    openImagePicker();
+                } else {
+                    ActivityCompat.requestPermissions(SettingsActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION);
+                }
+            }
+        });
+    
+
+   
+
         //click listeners and intent setting
         backArrowImage.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -39,8 +79,91 @@ public class SettingsActivity extends AppCompatActivity{
             }
         });
 
-        
+   saveSettingText.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        // Get the input values from EditText fields
+        String userName = userNameEdit.getText().toString().trim();
+        String phoneNumber = phoneNumberEdit.getText().toString().trim();
+        String locality = localityEdit.getText().toString().trim();
+
+        // Get the user ID
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        // Initialize the DatabaseReference for the user
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+        DatabaseReference userRef = usersRef.child(userId);
+
+        // Save the input values to the user's profile in Firebase Realtime Database
+        userRef.child("userName").setValue(userName);
+        userRef.child("phoneNumber").setValue(phoneNumber);
+        userRef.child("locality").setValue(locality);
+
+        // Show a success message or perform any additional actions
+        Toast.makeText(SettingsActivity.this, "Profile updated successfully", Toast.LENGTH_SHORT).show();
+    }
+});
 
         
     }
+
+     private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_CODE_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            uploadProfilePicture(imageUri);
+        }
+    }
+
+    private void uploadProfilePicture(Uri imageUri) {
+        if (imageUri != null) {
+            StorageReference profileImageRef = storageReference.child("profileImages/user.jpg");
+            profileImageRef.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(SettingsActivity.this, "Profile picture uploaded", Toast.LENGTH_SHORT).show();
+                            // TODO: Update the user's profile with the new image URL
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(SettingsActivity.this, "Failed to upload profile picture", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSION && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openImagePicker();
+        } else {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+     private String getCurrentUserId() {
+
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            if (currentUser != null) {
+                  String userId = currentUser.getUid();
+                   return userId;
+          } else {
+        // User is not authenticated or session expired
+        // Handle the situation accordingly
+        return null;
+        }
+   }
+
 }
+
+   
