8d02273c2c9bfa7506e8df82b93dbf9ff941c77c
OmentaElvis <omentaelvis@gmail.com>
2023-07-08 03:11:21 +0300

    Fixes on login sequence

--- app/java/com/healthcare/docconnect/AuthActivity.java
+++ app/java/com/healthcare/docconnect/AuthActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.content.Intent;
 
 import com.parse.ParseUser;
 import com.parse.LogInCallback;
@@ -35,6 +38,14 @@ public class AuthActivity extends AppCompatActivity implements View.OnClickListe
   // signin 
   EditText signinUsername;
   EditText signinPassword;
+
+  // errors 
+  TextView signupErrors;
+  TextView signinErrors;
+
+  // Progress bars 
+  ProgressBar signinProgress;
+  ProgressBar signupProgress;
   
   static final int BTN_OR_SIGNUP = 0x01;
   static final int BTN_OR_SIGNIN = 0x02;
@@ -65,6 +76,12 @@ public class AuthActivity extends AppCompatActivity implements View.OnClickListe
     signinUsername = (EditText) findViewById(R.id.signin_username);
     signinPassword = (EditText) findViewById(R.id.signin_password);
 
+    signinErrors = (TextView) findViewById(R.id.signin_errors);
+    signupErrors = (TextView) findViewById(R.id.signup_errors);
+
+    signinProgress = (ProgressBar) findViewById(R.id.signin_progress);
+    signupProgress = (ProgressBar) findViewById(R.id.signup_progress);
+
 
     btnOrSignIn.setTag(BTN_OR_SIGNIN);
     btnOrSignUp.setTag(BTN_OR_SIGNUP);
@@ -85,6 +102,22 @@ public class AuthActivity extends AppCompatActivity implements View.OnClickListe
     }else{
       signupLayout.setVisibility(View.GONE);
       signinLayout.setVisibility(View.VISIBLE);
+    }
+  }
+
+  private void showSignupProgress(boolean show){
+    if(show){
+      signupProgress.setVisibility(View.VISIBLE);
+    }else{
+      signupProgress.setVisibility(View.INVISIBLE);
+    }
+  }
+
+  private void showSigninProgress(boolean show) {
+    if(show){
+      signinProgress.setVisibility(View.VISIBLE);
+    }else{
+      signinProgress.setVisibility(View.INVISIBLE);
     }
   }
 
@@ -112,16 +145,19 @@ public class AuthActivity extends AppCompatActivity implements View.OnClickListe
   }
 
   public void login(){
+    signinErrors.setText("");
+    showSigninProgress(true);
     String username = signinUsername.getText().toString().trim();
     String password = signinPassword.getText().toString().trim();
     ParseUser.logInInBackground(username, password, new LogInCallback(){
       @Override 
       public void done(ParseUser user, ParseException e){
+        showSigninProgress(false);
         if (e == null){
-          Log.v("DocconnectDebug", ""+user);
-
+           Intent intent = new Intent(AuthActivity.this, HomeActivity.class);
+           startActivity(intent);
         }else{
-          Log.v("DocconnectDebug", ""+e);
+           signinErrors.setText(e.getMessage());
         }
 
       }
@@ -129,6 +165,7 @@ public class AuthActivity extends AppCompatActivity implements View.OnClickListe
 
   }
   public void signup(){
+    signupErrors.setText("");
     String username = signupUsername.getText().toString().trim();
     String firstName = signupFirstName.getText().toString().trim();
     String lastName = signupLastName.getText().toString().trim();
@@ -137,6 +174,25 @@ public class AuthActivity extends AppCompatActivity implements View.OnClickListe
     String confirmPassword = signupConfirmPassword.getText().toString().trim();
     String phoneNumber = signupPhone.getText().toString().trim();
 
+    if(username == ""){
+      signupErrors.setText("Username cannot be null");
+      return;
+    } else if(firstName == "" || lastName == ""){
+      signupErrors.setText("First and Last name is required.");
+      return;
+    }else if(email == ""){
+      signupErrors.setText("Email is required. ");
+      return;
+    }else if(password == ""){
+      signupErrors.setText("password is required");
+      return;
+    }else if(!password.equals(confirmPassword)){
+      signupErrors.setText("Passwords do not match. Please Confirm.");
+      return;
+    }else if(phoneNumber == ""){
+      signupErrors.setText("Phone number is required.");
+      return;
+    }
     ParseUser user = new ParseUser();
     user.setUsername(username);
     user.setEmail(email);
@@ -144,15 +200,17 @@ public class AuthActivity extends AppCompatActivity implements View.OnClickListe
     user.put("firstname", firstName);
     user.put("lastname", lastName);
     user.put("phonenumber", phoneNumber);
+    showSignupProgress(true);
+
     user.signUpInBackground(new SignUpCallback(){
       @Override
       public void done(ParseException e){
+        showSignupProgress(false);
         if(e == null){
-          Log.v("DocconnectDebug", "Sign Up success");
-
+           Intent intent = new Intent(AuthActivity.this, HomeActivity.class);
+           startActivity(intent);
         }else{
-          Log.e("DocconnectDebug", ""+e);
-
+           signupErrors.setText(e.getMessage());
         }
       }
     });
